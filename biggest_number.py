b03569f701fc4406504a6fe99217f40fe1b5b262
trushithanama <67234326+trushithanama@users.noreply.github.com>
2020-06-22 20:10:59 +0530

    Create biggest_number.py

---
+++ biggest_number.py
@@ -0,0 +1,14 @@
+# find maximum number
+def biggest(a,b,c):
+    if a>b and a>c :
+      print("biggest number=",a)
+  elif b>a and b>c:  
+       print("biggest number=",b)
+  elif a==b:
+       print("a=b")
+  elif b==c:
+       print("b==c")
+   else:
+       print("biggest number=",c)
+   
+   
