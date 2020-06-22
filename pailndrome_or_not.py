c2ec58826831ba73f54db6b5ee2a84851e087656
trushithanama <67234326+trushithanama@users.noreply.github.com>
2020-06-22 20:56:03 +0530

    Create pailndrome_or_not.py

---
+++ pailndrome_or_not.py
@@ -0,0 +1,9 @@
+#paildrome or not 
+def fileinput(filename)
+ palindrome = flase
+ try:
+ if not palindrome:
+     print("no palindromes found for length entered.")
+ except:
+     print("no palindromes found for length entered.")
+ finally:
